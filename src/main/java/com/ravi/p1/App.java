555c7cecb1a6f6af5df21924d087013b520491e7
ravi2krishna <ravi2krishna@gmail.com>
2020-07-23 15:00:00 +0530

    cal implemented

--- src/main/java/com/ravi/p1/App.java
+++ src/main/java/com/ravi/p1/App.java
@@ -1,13 +1,59 @@
 package com.ravi.p1;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
- public class App  
+
+
+public class App  
 {
-    public static void main( String[] args )
+   
+	private int num1;
+	private int num2;
+	
+	
+    public int getNum1() {
+		return num1;
+	}
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
+	public int getNum2() {
+		return num2;
+	}
+
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+
+	public int sum(int num1, int num2) {
+		return num1+num2;	
+	}
+	
+	public int product(int num1, int num2) {
+		return num1*num2;	
+	}
+	
+
+	public static void main( String[] args )
     {
-        System.out.println( "Development testing web hook trigger" );
+		App cal = new App();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter First Number");
+		int num1 = scan.nextInt();
+		
+		System.out.println("Enter Second Number");
+		int num2 = scan.nextInt();
+		
+		System.out.println("The Sum is : "+cal.sum(num1, num2));
+		
+		System.out.println("The Product is : "+cal.product(num1, num2));
+		
     }
 }
